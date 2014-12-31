85cc06e4652806d197823a00c197acb54b12dc30
snoopdesigns <snoopdesigns@gmail.com>
2014-12-31 19:25:29 +0300

    Added unit test

---
+++ src/test/java/org/snoopdesigns/roadtraffic/NetworkTest.java
@@ -0,0 +1,10 @@
+package org.snoopdesigns.roadtraffic;
+
+import org.snoopdesigns.roadtraffic.nnetwork.RoadTrafficPredictionPerceptron;
+
+public class NetworkTest {
+
+    public void testNetwork() {
+        RoadTrafficPredictionPerceptron.test();
+    }
+}
